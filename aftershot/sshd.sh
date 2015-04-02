551799cc4ed2c359edf34ce585518c24e6fb058e
Ky-Anh Huynh <kyanh@theslinux.org>
2015-04-02 15:35:45 +0700

    aftershot/*: add

---
+++ aftershot/sshd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ -z "$_UID" && -z "$_GID" ]]; then
+  echo >&2 ":: You must specifiy _UID and _GID when executing 'docker run'"
+  exit 1
+fi
+
+groupmod -g $_GID aftershot
+usermod -u $_UID -g $_GID aftershot
+
+exec /usr/sbin/sshd -D
